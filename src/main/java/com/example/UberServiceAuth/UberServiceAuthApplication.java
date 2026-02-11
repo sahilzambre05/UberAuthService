1804b50fee24bd206c9e4919ccc11825d6f78525
sahilzambre05 <sahildinesh.zambre2023@vitstudent.ac.in>
2026-02-11 22:46:05 +0530

    Setup for Auth Service

---
+++ src/main/java/com/example/UberServiceAuth/UberServiceAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.UberServiceAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberServiceAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberServiceAuthApplication.class, args);
+	}
+
+}
