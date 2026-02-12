c96c5f99e515bc28088eb5d4f60e90d01923af35
sahilzambre05 <sahildinesh.zambre2023@vitstudent.ac.in>
2026-02-12 10:07:55 +0530

    Copied migrations and models and created a new migration for adding phone number

---
+++ src/main/java/com/example/UberServiceAuth/models/Passenger.java
@@ -0,0 +1,25 @@
+package com.example.UberServiceAuth.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler" , "bookings"})
+public class Passenger extends BaseModel{
+    private String name;
+
+
+    @OneToMany(mappedBy = "passenger")
+    @Builder.Default
+    private List<Booking> bookings = new ArrayList<>();
+}
