42978cbf79adc973966c29ecc80c59eeeb571685
sahilzambre05 <sahildinesh.zambre2023@vitstudent.ac.in>
2026-02-13 15:20:22 +0530

    JWT creation started

---
+++ src/main/java/com/example/UberServiceAuth/services/JwtService.java
@@ -0,0 +1,46 @@
+package com.example.UberServiceAuth.services;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService implements CommandLineRunner {
+    @Value("${jwt.expiry}")
+    private int expiry;
+
+    @Value("${jwt.secret}")
+    private String SECRET;
+
+    public String createToken(Map<String, Object> payload, String email) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiry*1000L);
+        SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+        return Jwts.builder()
+                .claims(payload)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(expiryDate)
+                .subject(email)
+                .signWith(key)
+                .compact();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Map<String, Object> mp = new HashMap<>();
+        mp.put("email", "a@b.com");
+        mp.put("phoneNumber", "9999999999");
+        String result = createToken(mp, "sanket");
+        System.out.println("Generated token is: " + result);
+//        System.out.println(extractPayload(result, "email").toString());
+    }
+}
