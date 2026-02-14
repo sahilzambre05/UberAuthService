edc7cca1d90ed698032570d9c534df26be91d093
sahilzambre05 <sahildinesh.zambre2023@vitstudent.ac.in>
2026-02-14 12:46:19 +0530

    Added JwtAuthFilter

--- src/main/java/com/example/UberServiceAuth/configurations/SpringSecurity.java
+++ src/main/java/com/example/UberServiceAuth/configurations/SpringSecurity.java
@@ -1,7 +1,9 @@
 package com.example.UberServiceAuth.configurations;
 
 
+import com.example.UberServiceAuth.filter.JwtAuthFilter;
 import com.example.UberServiceAuth.services.UserDetailServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,12 +16,16 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity implements WebMvcConfigurer {
+
+    @Autowired
+    public JwtAuthFilter jwtAuthFilter;
 
 
     @Bean
@@ -36,8 +42,10 @@ public class SpringSecurity implements WebMvcConfigurer {
                         auth
                                 .requestMatchers("/api/v1/auth/signup/*").permitAll()
                                 .requestMatchers("/api/v1/auth/signin/*").permitAll()
-                                .requestMatchers("/api/v1/auth/validate").permitAll()
+                                .requestMatchers("/api/v1/auth/validate").authenticated()
                 )
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
